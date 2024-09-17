106125157bb140d14ada1c45f81544fcf613232c
ujjwal bhaedwaj <ujjwalbhardwaj.2002>
2024-09-17 18:06:20 +0530

    continue

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import authService from "./appwrite/auth";
 import { login, logout } from "./store/authSlice";
 import { Footer, Header } from "./components";
 import { Outlet } from "react-router-dom";
+import DarkGridBackground from './components/container/Container'
 
 function App() {
   const [loading, setLoading] = useState(true);
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,11 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import LogoutButton from "./Header/LogoutButton";
 
 export {
     Header,
     Footer,
+    Container,
+    LogoutButton,
 }
