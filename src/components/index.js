e164f2ce5b4bb7bbdf91ae2b25c36f85d743593a
ujjwal bhaedwaj <ujjwalbhardwaj.2002>
2024-09-18 18:46:16 +0530

    half way

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,16 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import LogoutButton from "./Header/LogoutButton";
+import Button from './Button'
+import Input from './Input'
+import Select from './Select'
 
 export {
     Header,
     Footer,
     Container,
     LogoutButton,
+    Button,
+    Input,
+    Select,
 }
